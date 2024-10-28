838dd91344795062515b053d9326518fb67c5798
wtto00 <wtto00@outlook.com>
2024-10-28 19:16:06 +0800

    cargo fmt

--- src/mobile.rs
+++ src/mobile.rs
@@ -1,7 +1,7 @@
 use serde::de::DeserializeOwned;
 use tauri::{
-  plugin::{PluginApi, PluginHandle},
-  AppHandle, Runtime,
+    plugin::{PluginApi, PluginHandle},
+    AppHandle, Runtime,
 };
 
 #[cfg(target_os = "ios")]
@@ -9,14 +9,15 @@ tauri::ios_plugin_binding!(init_plugin_app_events);
 
 // initializes the Kotlin or Swift plugin classes
 pub fn init<R: Runtime, C: DeserializeOwned>(
-  _app: &AppHandle<R>,
-  api: PluginApi<R, C>,
+    _app: &AppHandle<R>,
+    api: PluginApi<R, C>,
 ) -> crate::Result<AppEvents<R>> {
-  #[cfg(target_os = "android")]
-  let handle = api.register_android_plugin("wang.tato.tauri_plugin_app_events", "AppEventsPlugin")?;
-  #[cfg(target_os = "ios")]
-  let handle = api.register_ios_plugin(init_plugin_app_events)?;
-  Ok(AppEvents(handle))
+    #[cfg(target_os = "android")]
+    let handle =
+        api.register_android_plugin("wang.tato.tauri_plugin_app_events", "AppEventsPlugin")?;
+    #[cfg(target_os = "ios")]
+    let handle = api.register_ios_plugin(init_plugin_app_events)?;
+    Ok(AppEvents(handle))
 }
 
 /// Access to the app-events APIs.
